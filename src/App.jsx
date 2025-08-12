59384061c95218eeb67702deb461f41782e0af5b
Woojin <119497359+WWOJJIN@users.noreply.github.com>
2025-08-12 12:12:07 +0900

    cncn

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import WeatherCard from './components/WeatherCard'
 import { useState, useRef, useEffect } from 'react'
 import { fetchCordinates } from './api/geo'
 import { fetchWeatherByCoords } from './api/weather'
+import { getColorByWeatherId } from './api/bgColor'
 
 function App() {
 
@@ -12,6 +13,12 @@ function App() {
   const [loading, setLoading] = useState(false)
   const [err, setErr] = useState('')
   const inputRef = useRef(null)
+
+
+  const bg = weather?.weather?.[0]?.id
+    ? getColorByWeatherId(weather.weather[0].id)
+    : 'linear-gradient(135deg, #FFFFFF 0%, #F1F5F9 100%)';
+
 
   useEffect(() => {
     inputRef.current.focus()
@@ -50,23 +57,26 @@ function App() {
   }
 
   return (
-    <div className='app'>
-      <h1>우진이의 날씨앱</h1>
-      <div className="input-wrap">
-        <input
-          ref={inputRef}
-          value={city}
-          onChange={onChangeInput}
-          onKeyUp={onKeyup}
-          type="text"
-          placeholder='도시이름을 입력하세요' />
-        <button onClick={handleSearch} disabled={loading}>{loading ? "검색중..." : "검색"}</button>
-      </div>
-      {err && <p className='error'>{err}</p>}
-      {loading && <p className='info'>불러오는중...</p>}
+    <section style={{ background: bg, minHeight: '100vh', transition: 'background .3s ease' }}>
+      <div className='app' ></div>
+      <div className='app'>
+        <h1>우진이의 날씨앱</h1>
+        <div className="input-wrap">
+          <input
+            ref={inputRef}
+            value={city}
+            onChange={onChangeInput}
+            onKeyUp={onKeyup}
+            type="text"
+            placeholder='도시이름을 입력하세요' />
+          <button onClick={handleSearch} disabled={loading}>{loading ? "검색중..." : "검색"}</button>
+        </div>
+        {err && <p className='error'>{err}</p>}
+        {loading && <p className='info'>불러오는중...</p>}
 
-      <WeatherCard weather={weather} />
-    </div>
+        <WeatherCard weather={weather} />
+      </div>
+    </section>
   )
 }
 
